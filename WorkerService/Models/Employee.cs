e2fd10961744e2ed177218663338d345318d53cb
PreCho01 <precho01@outlook.com>
2025-05-27 14:48:02 +0530

    cmt

---
+++ WorkerService/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿public class Employee
+{
+    public int Emp_id { get; set; }
+    public string? Emp_name { get; set; }
+    public string? Emp_department { get; set; }
+
+    //public List<string> Skills { get; set; }
+    //public Dictionary<string, string> Projects { get; set; }
+}

c8abb0ba3adbc5dd95781777ad16e6aef3d14d69
PreCho01 <precho01@outlook.com>
2025-05-29 09:49:26 +0530

    added emp model

--- WorkerService/Models/StudentProfile.cs
+++ WorkerService/Models/StudentProfile.cs
@@ -7,7 +7,6 @@ namespace WorkerService.Models
         public int StudentId { get; set; }
         public string? Name { get; set; }
         public string? Department { get; set; }
-
         public List<string>? EnrolledCourses { get; set; }
         public Dictionary<string, int>? SubjectScores { get; set; }
     }
